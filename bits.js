c23ab3cbf320557577a5dea642f5c1a25631c105
mierenhoop <46939409+mierenhoop@users.noreply.github.com>
2024-02-10 16:43:23 +0100

    initial

---
+++ bits.js
@@ -0,0 +1,151 @@
+/**
+ * Alias for `document.querySelector`.
+ * @param {string} s 
+ * @returns {HTMLElement | null}
+ */
+function $(s) {
+    return document.querySelector(s);
+}
+
+/**
+ * Encode the contents of a form element for use as POST payload.
+ * Only supported by modern browsers.
+ * @param {HTMLFormElement} form 
+ * @returns {URLSearchParams}
+ */
+function encForm(form) {
+    // @ts-ignore
+    return new URLSearchParams(new FormData(form));
+}
+
+/**
+ * Encode form inputs into query string.
+ * IE8+ compatible.
+ * @param {HTMLFormElement} form element which contains
+ * @param {(string | [string, string])[]} keys array of either name attributes for input elements or 
+ * @returns {string} query string
+ */
+function _encForm(form, keys) {
+    var fields, key, s = "";
+    for (var i = 0; i < keys.length; i++) {
+        key = keys[i];
+        if (typeof key == "string")
+            fields = form.querySelectorAll("[name=\"" + key + "\"]");
+        else
+            key = key[0], fields = [{ value: key[1] }];
+        for (var j = 0; j < fields.length; j++) { // @ts-ignore
+            s += (i + j ? "&" : "?") + encodeURIComponent(key) + "=" + encodeURIComponent(fields[i].value);
+        }
+    }
+    return s;
+}
+
+/**
+ * @typedef Context
+ * @type {object}
+ * @property {XMLHttpRequest} xhr
+ * @property {HTMLElement | null} el
+ * @property {((f: ((ctx: Context) => void) | undefined) => void)} call
+ */
+
+/**
+ * IE7+ compatible.
+ * @param {string | [string, XMLHttpRequestBodyInit]} req
+ * request URL, optionally prefixed with request method like in HTTP
+ * @param {HTMLElement | null} target target element,
+ * if target element is null, the swap callback will still be fired if existant.
+ * @param {"outerHTML" | "innerHTML" | "beforebegin" | "afterbegin" | "beforeend" | "afterend" | "delete" |"none" } [swapMethod]
+ * what to do with received element, in the case of the callback, swapping should be done manually.
+ * default is "innerHTML".
+ * @param {((ctx: Context) => void)} [f]
+ */
+function swap(req, target, swapMethod, f) {
+    var data;
+    if (typeof req == "object")
+        data = req[1], req = req[0];
+    if (!swapMethod) swapMethod = "innerHTML";
+    var xhr = new XMLHttpRequest();
+    var s = req.split(" ");
+    xhr.open(s[1] ? s[0] : "GET", s[1] || s[0]);
+    xhr.onload = function () {
+        /** @type {Context} */
+        var ctx = {
+            xhr: xhr,
+            el: target,
+            call: function (f) {
+                if (f) f(this);
+                else if (this.el) { // @ts-ignore
+                    if (swapMethod.match("(inner|outer)HTML")) // @ts-ignore
+                        this.el[swapMethod] = this.xhr.responseText; // @ts-ignore
+                    else if (swapMethod.match("(after|before)(begin|end)")) // @ts-ignore
+                        this.el.insertAdjacentHTML(swapMethod, this.xhr.responseText);
+                    else if (swapMethod == "delete" && this.el.parentNode)
+                        this.el.parentNode.removeChild(this.el);
+                }
+            },
+        };
+        ctx.call(f);
+    };
+    xhr.send(data);
+}
+
+function onLoad(f) {
+    window.addEventListener("load", f);
+}
+
+/**
+ * 
+ * @param {((ctx: Context) => void)} [f]
+ * @returns {(ctx: Context) => void}
+ */
+function nothingIfError(f) {
+    return function (ctx) {
+        if (ctx.xhr.status != 200) return;
+        ctx.call(f);
+    }
+}
+
+/**
+ * @param {number} ms
+ * @param {((ctx: Context) => void)} [f]
+ * @returns {(ctx: Context) => void}
+ */
+function delay(ms, f) {
+    return function (ctx) {
+        setTimeout(ctx.call, ms, f);
+    }
+}
+
+/**
+ * @param {number} ms
+ * @param {((ctx: Context) => void)} [f]
+ * @returns {(ctx: Context) => void}
+ */
+function useSettle(ms, f) {
+    return function (ctx) {
+        ctx.call(f);
+        if (ctx.el) {
+            ctx.el.classList.add("settling");
+            setTimeout(ctx.el.classList.remove, ms, "settling");
+        }
+    }
+}
+/**
+ * @param {((ctx: Context) => void)} [f]
+ * @returns {(ctx: Context) => void}
+ */
+function useTitle(f) {
+    return function (ctx) {
+        var parent = ctx.el && ctx.el.parentElement;
+        ctx.call(f);
+        if (parent) {
+            var title = parent.querySelector("title");
+            if (title) {
+                if (title.textContent)
+                    document.title = title.textContent; // @ts-ignore
+                title.parentNode.removeChild(title);
+            }
+        }
+    }
+}
+
